3086edd177d413f5485063867331b565fe0e913e
MassiBr <massikaneki09@gmail.com>
2023-12-28 00:15:07 +0100

    implémentation de l'algo sudokuSolver

--- backtrack.py
+++ backtrack.py
@@ -2,8 +2,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from tkinter import messagebox
 
-# Utiliser une palette de trois couleurs : rouge, vert, bleu
-node_colors = ["red", "green"]
 
 # Exemple d'utilisation avec le graphe que vous avez donné
 # monGraphe = {
@@ -14,7 +12,8 @@ node_colors = ["red", "green"]
 #     5: [3, 4],
 # }
 
-monGraphe = {1: [2, 3], 2: [1, 3], 3: [1, 2]}
+# monGraphe = {1: [2, 3], 2: [1, 3], 3: [1, 2]}
+# node_colors = ["red", "green"]
 
 
 def isSafeToColor(graphe, vertex, color, sol):
@@ -35,68 +34,71 @@ def backtrack(graphe, vertex, colors, solution):
     if vertex is None:
         return True
 
-    print(f"Coloration de sommet {vertex} avec les couleurs disponibles : {colors} \n")
+    # print(f"Coloration de sommet {vertex} avec les couleurs disponibles : {colors} \n")
     for col in colors:
         if isSafeToColor(graphe, vertex, col, solution):
             solution[vertex] = col
 
             next_vertex = getNextVertex(graphe, solution)
-            print(
-                f"   Essai de couleur {col} pour le sommet {vertex}. Solution actuelle : {solution}\n"
-            )
+            # print(
+            #     f"   Essai de couleur {col} pour le sommet {vertex}. Solution actuelle : {solution}\n"
+            # )
             if backtrack(graphe, next_vertex, colors, solution):
                 return True
 
             solution[
                 vertex
             ] = None  # Revenir en arrière (backtrack) en annulant la couleur
-            print(
-                f"   Retour en arrière. Annulation de la couleur pour le sommet {vertex}. Solution actuelle : {solution}\n"
-            )
+    #         print(
+    #             f"   Retour en arrière. Annulation de la couleur pour le sommet {vertex}. Solution actuelle : {solution}\n"
+    #         )
 
-    print(f"Aucune couleur n'est valide pour le sommet {vertex}.\n")
+    # print(f"Aucune couleur n'est valide pour le sommet {vertex}.\n")
     return False
 
 
-first_vertex = list(monGraphe.keys())[0]
-vertex_color = {v: None for v in monGraphe}
+# # Utiliser une palette de trois couleurs : rouge, vert, bleu
+# node_colors = ["red", "green", "blue"]
+
+# first_vertex = list(monGraphe.keys())[0]
+# vertex_color = {v: None for v in monGraphe}
 
 
-def getSol():
-    if backtrack(monGraphe, first_vertex, node_colors, vertex_color):
-        return vertex_color
-    return None
+# def getSol():
+#     if backtrack(monGraphe, first_vertex, node_colors, vertex_color):
+#         return vertex_color
+#     return None
 
 
-sol_from_backtrack = getSol()
-
-# -----------------------------------------affichage-------------------------------------
-
-
-def draw_colored_graph(graph, colors):
-    G = nx.from_dict_of_lists(graph)
-
-    # Dessiner le graphe avec les couleurs attribuées
-    pos = nx.spring_layout(G)
-    node_colors = [colors[sommet] for sommet in G.nodes]
-    nx.draw(
-        G,
-        pos,
-        with_labels=True,
-        node_color=node_colors,
-        font_color="white",
-        font_size=10,
-        font_family="Arial",
-    )
-
-    # Affichage
-    plt.show()
+# sol_from_backtrack = getSol()
+# print(sol_from_backtrack)
+# # -----------------------------------------affichage-------------------------------------
 
 
-# Dessiner le graphe coloré
-if sol_from_backtrack is None:
-    messagebox.showerror(
-        "Erreur", "Aucune solution trouvée. Le graphe ne peut pas être coloré."
-    )
-else:
-    draw_colored_graph(monGraphe, sol_from_backtrack)
+# def draw_colored_graph(graph, colors):
+#     G = nx.from_dict_of_lists(graph)
+
+#     # Dessiner le graphe avec les couleurs attribuées
+#     pos = nx.spring_layout(G)
+#     node_colors = [colors[sommet] for sommet in G.nodes]
+#     nx.draw(
+#         G,
+#         pos,
+#         with_labels=True,
+#         node_color=node_colors,
+#         font_color="white",
+#         font_size=10,
+#         font_family="Arial",
+#     )
+
+#     # Affichage
+#     plt.show()
+
+
+# # Dessiner le graphe coloré
+# if sol_from_backtrack is None:
+#     messagebox.showerror(
+#         "Erreur", "Aucune solution trouvée. Le graphe ne peut pas être coloré."
+#     )
+# else:
+#     draw_colored_graph(monGraphe, sol_from_backtrack)
--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,79 +1,16 @@
-import numpy as np
-import time
-
-
-# colors_dict = {
-#     1: "red",
-#     2: "blue",
-#     3: "green",
-#     4: "purple",
-#     5: "orange",
-#     6: "brown",
-#     7: "pink",
-#     8: "cyan",
-#     9: "gray",
-# }
-# # Exemple d'utilisation
-# for number, color in colors_dict.items():
-#     print(f"Number {number} is associated with the color {color}")
-
-
-# grid = [
-#     [0, 0, 8, 0, 2, 0, 0, 0, 9],
-#     [0, 0, 7, 0, 0, 0, 4, 0, 2],
-#     [2, 0, 0, 7, 9, 0, 0, 1, 0],
-#     [0, 0, 9, 0, 0, 5, 3, 0, 6],
-#     [0, 6, 0, 0, 4, 0, 8, 9, 0],
-#     [7, 0, 4, 9, 0, 0, 1, 0, 0],
-#     [0, 7, 0, 0, 3, 6, 0, 0, 1],
-#     [5, 0, 3, 0, 0, 0, 7, 0, 0],
-#     [6, 0, 0, 0, 5, 0, 2, 8, 0],
-# ]
-
-
-# def est_possible(grille, ligne, colonne, valeur):
-#     # test de la ligne
-#     for i in range(9):
-#         if grille[ligne][i] == valeur:
-#             return False
-#     # test de la colonne
-#     for i in range(9):
-#         if grille[i][colonne] == valeur:
-#             return False
-#     # test du bloc
-#     # la case en haut à gauche de bloc a pour coordonnées
-#     # (3 * (ligne // 3), 3 * (colonne // 3))
-#     for i in range(3 * (ligne // 3), 3 * (ligne // 3) + 3):
-#         for j in range(3 * (colonne // 3), 3 * (colonne // 3) + 3):
-#             if grille[i][j] == valeur:
-#                 return False
-#     return True
-
-
-# def est_possible(grille, ligne, colonne, valeur):
-#     # test de la ligne
-#     for i in range(4):
-#         if grille[ligne][i] == valeur:
-#             return False
-#     # test de la colonne
-#     for i in range(4):
-#         if grille[i][colonne] == valeur:
-#             return False
-#         # test du bloc
-#     bloc_ligne = 2 * (ligne // 2)
-#     bloc_colonne = 2 * (colonne // 2)
-#     for i in range(bloc_ligne, bloc_ligne + 2):
-#         for j in range(bloc_colonne, bloc_colonne + 2):
-#             if grille[i][j] == valeur:
-#                 return False
-#     return True
+import backtrack as bkt
+import networkx as nx
+import matplotlib.pyplot as plt
+from tkinter import messagebox, ttk
+import tkinter as tk
+import scipy as sp
 
 
 def assign_indices():
     indices = {}
     count = 0
-    for x in range(4):
-        for y in range(4):
+    for x in range(9):
+        for y in range(9):
             indices[(x, y)] = count
             count += 1
     return indices
@@ -81,30 +18,29 @@ def assign_indices():
 
 def graph_from_grid():
     graph = {}
-
     # Ajouter tous les sommets au graphe et bien les indicés
     indices = assign_indices()
 
     # Ajouter les voisins pour chaque sommet en respectant les règles du Sudoku
-    for x in range(4):
-        for y in range(4):
+    for x in range(9):
+        for y in range(9):
             neighbors = set()
 
             # Ajouter les voisins de la même ligne
-            for i in range(4):
+            for i in range(9):
                 if i != y:
                     neighbors.add(indices[(x, i)])
 
             # Ajouter les voisins de la même colonne
-            for j in range(4):
+            for j in range(9):
                 if j != x:
                     neighbors.add(indices[(j, y)])
 
             # Ajouter les voisins du même carré (2x2)
-            square_start_x = 2 * (x // 2)
-            square_start_y = 2 * (y // 2)
-            for i in range(square_start_x, square_start_x + 2):
-                for j in range(square_start_y, square_start_y + 2):
+            square_start_x = 3 * (x // 3)
+            square_start_y = 3 * (y // 3)
+            for i in range(square_start_x, square_start_x + 3):
+                for j in range(square_start_y, square_start_y + 3):
                     if i != x and j != y:
                         neighbors.add(indices[(i, j)])
 
@@ -114,39 +50,170 @@ def graph_from_grid():
     return graph
 
 
-# Exemple d'utilisation
-grille = [[3, 0, 0, 0], [0, 1, 3, 0], [0, 4, 2, 0], [0, 0, 0, 4]]
 monGraphe = graph_from_grid()
-
 # Affichage du graphe
-for sommet, voisins in monGraphe.items():
-    print(f"{sommet} : {voisins}")
+# for sommet, voisins in monGraphe.items():
+#     print(f"{sommet} : {voisins}")
+
+# Exemple d'utilisation
+grille = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+]
+
+colors_dict = {
+    1: "red",
+    2: "blue",
+    3: "green",
+    4: "black",
+    5: "grey",
+    6: "orange",
+    7: "purple",
+    8: "yellow",
+    9: "pink",
+}
 
 
-# def solver(grille):
-#     for x in range(4):
-#         for y in range(4):
-#             if grille[x][y] == 0:
-#                 for n in range(1, 5):
-#                     if est_possible(grille, x, y, n):
-#                         grille[x][y] = n
-#                         if solver(grille):
-#                             return True
-#                         grille[x][y] = 0
-#                 return False
-#     print(np.matrix(grille))
-#     return True
+def sol():
+    first_vertex = list(monGraphe.keys())[0]
+    vertex_color = {v: None for v in monGraphe}
+
+    for value, color_name in colors_dict.items():
+        # Trouver toutes les positions où la valeur est présente dans la grille
+        positions = [
+            (x, y) for x in range(9) for y in range(9) if grille[x][y] == value
+        ]
+        # Attribuer la couleur à ces positions
+        for p in positions:
+            vertex_color[assign_indices()[p]] = value  # Utiliser la valeur comme clé
+
+    if bkt.backtrack(monGraphe, first_vertex, colors_dict.keys(), vertex_color):
+        # Correction lors de l'impression du résultat
+        result = {
+            k: colors_dict[v] if v is not None else None
+            for k, v in vertex_color.items()
+        }
+        return result
+    return None
 
 
-# g = [[3, 0, 0, 0], [0, 1, 3, 0], [0, 4, 2, 0], [0, 0, 0, 4]]
+sol_from_backtrack = sol()
+print(sol_from_backtrack)
 
 
-# solver(grid)
+def inverse_dict(d):
+    return {v: k for k, v in d.items()}
 
 
-# start_time = time.time()
-# sudoku_grid = generate_sudoku()
-# generation_time = time.time() - start_time
+colors_inv = inverse_dict(colors_dict)
+
+# Affichage du Sudoku initial
+print("Sudoku initial :")
+for row in grille:
+    print(" ".join(map(str, row)))
 
 
-# print("Solving Time: %.6f seconds" % generation_time)
+# Afficher la solution du Sudoku
+print("Solution du Sudoku :")
+for i in range(9):
+    for j in range(9):
+        print(colors_inv[sol_from_backtrack[assign_indices()[(i, j)]]], end=" ")
+    print()
+
+# # -----------------------------------------affichage-------------------------------------
+
+
+def draw_colored_graph(graph, colors):
+    G = nx.from_dict_of_lists(graph)
+    # Dessiner le graphe avec les couleurs attribuées
+    pos = nx.kamada_kawai_layout(G)
+    node_colors = [
+        colors.get(sommet, 0) for sommet in G.nodes
+    ]  # Utilisez les couleurs attribuées
+    nx.draw(
+        G,
+        pos,
+        with_labels=True,
+        node_color=node_colors,
+        font_color="white",
+        font_size=8,
+        font_family="Arial",
+    )
+    # Affichage
+    plt.show()
+
+
+# Dessiner le graphe coloré
+if sol_from_backtrack is None:
+    messagebox.showerror(
+        "Erreur", "Aucune solution trouvée. Le graphe ne peut pas être coloré."
+    )
+else:
+    draw_colored_graph(monGraphe, sol_from_backtrack)
+
+
+# # Affichage de la grille Sudoku résolue
+# class SudokuGUI:
+#     def __init__(self, master, sudoku_initial, sudoku_solution):
+#         self.master = master
+#         self.master.title("Sudoku Solver")
+#         self.sudoku_initial = sudoku_initial
+#         self.sudoku_solution = sudoku_solution
+#         self.create_grid()
+
+#     def create_grid(self):
+#         frame_initial = ttk.Frame(self.master)
+#         frame_initial.grid(row=0, column=0)
+
+#         frame_solution = ttk.Frame(self.master)
+#         frame_solution.grid(row=1, column=0)
+
+#         # Grille initiale
+#         ttk.Label(frame_initial, text="Sudoku Initial", font=("Arial", 16)).grid(
+#             row=0, column=0, columnspan=4
+#         )
+#         canvas_initial = tk.Canvas(frame_initial, width=200, height=200)
+#         canvas_initial.grid(row=1, column=0, columnspan=4)
+#         self.draw_grid(canvas_initial, self.sudoku_initial)
+
+#         # Grille solution
+#         ttk.Label(frame_solution, text="Sudoku Solution", font=("Arial", 16)).grid(
+#             row=0, column=0, columnspan=4
+#         )
+#         canvas_solution = tk.Canvas(frame_solution, width=200, height=200)
+#         canvas_solution.grid(row=1, column=0, columnspan=4)
+#         self.draw_grid(canvas_solution, self.sudoku_solution)
+
+#     def draw_grid(self, canvas, grid):
+#         cell_size = 50
+#         for i in range(4):
+#             for j in range(4):
+#                 x1, y1 = j * cell_size, i * cell_size
+#                 x2, y2 = x1 + cell_size, y1 + cell_size
+#                 canvas.create_rectangle(x1, y1, x2, y2, outline="black")
+
+#                 if isinstance(grid, list):  # Pour la grille initiale (liste de listes)
+#                     value = grid[i][j]
+#                 elif isinstance(grid, dict):  # Pour la grille solution (dictionnaire)
+#                     value = grid[assign_indices()[(i, j)]]
+
+#                 if value != 0:
+#                     canvas.create_text(
+#                         (x1 + x2) / 2,
+#                         (y1 + y2) / 2,
+#                         text=str(value),
+#                         font=("Arial", 16),
+#                     )
+
+
+# if __name__ == "__main__":
+#     root = tk.Tk()
+#     app = SudokuGUI(root, grille, sudoku_solution)
+#     root.mainloop()
